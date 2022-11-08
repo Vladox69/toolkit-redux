8438e72161e72385b79201df9d04ee471cdfe1fd
Vladox69 <marwladiagua2001@hotmail.com>
2022-11-08 09:54:17 -0500

    Counter con redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment } from './store/slices/counter/counterSlices';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const {value}= useSelector(state=>state.counter);
+  const dispatch=useDispatch();
   return (
     <div className="App">
       <div>
@@ -17,16 +18,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={()=>dispatch(increment())}>
+          count is {value}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
